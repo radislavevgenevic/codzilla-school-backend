f6cafa5d2e1a6fecee384acee75bc7703d39ecf4
RadislaveTK <tkachenkoradislav@gmail.com>
2026-06-01 04:03:58 +0600

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://codzilla-school-backend.local', 'https://codzilla-school.up.railway.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://codzilla-school-backend.local', 'https://codzilla-school.up.railway.app', 'https://codzilla-school-backend-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
